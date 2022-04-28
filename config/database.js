2a09ba57b2804c3cad18d99a28d27cea82d800f2
Jakub <jakubmagacp@gmail.com>
2022-04-28 13:37:53 +0200

    new db

--- config/database.js
+++ config/database.js
@@ -2,10 +2,17 @@ const path = require('path');
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'postgres',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env('DATABASE_HOST', 'chunee.db.elephantsql.com/jnjsqtin'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'jnjsqtin'),
+      username: env('DATABASE_USERNAME', 'jnjsqtin'),
+      password: env('DATABASE_PASSWORD', ' oIIeHuqN9BNUCEX-K2W9NDnWEwcQRIU3'),
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), 
+      },
     },
-    useNullAsDefault: true,
+    debug: false,
   },
 });
